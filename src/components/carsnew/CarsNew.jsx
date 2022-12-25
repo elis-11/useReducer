884868e96e70ba393d31c65787886419523d0253
elis-11 <eliza.arzanukaeva@gmail.com>
2022-12-25 20:31:43 +0100

    Fixed Application

--- src/components/carsnew/CarsNew.jsx
+++ src/components/carsnew/CarsNew.jsx
@@ -17,10 +17,6 @@ export const CarsNew =() => {
     });
   }
 
-
-
-  
-
   if (productName) {
     filteredCars = filteredCars.filter((car) => {
       return car.name.toLowerCase().includes(productName.toLowerCase());
