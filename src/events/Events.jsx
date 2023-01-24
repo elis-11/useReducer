699f367601ba51e159dc5c394b47a8c10c9c72fa
elis-11 <eliza.arzanukaeva@gmail.com>
2023-01-24 13:05:31 +0100

    Objects - actions

--- src/events/Events.jsx
+++ src/events/Events.jsx
@@ -2,25 +2,21 @@ import { useState } from "react";
 
 export const Events = () => {
   // state for str DATA
-  const [strings, setStrings] = useState([
-    "apple",
-    "banana",
-    "cherry",
-  ]);
+  const [strings, setStrings] = useState(["apple", "banana", "cherry"]);
   const [stringNew, setStringNew] = useState("");
 
-  const [fruitObjects, setFruitObjects] = useState([
+  const [objects, setObjects] = useState([
     { _id: "f1", name: "apple" },
     { _id: "f2", name: "banana" },
     { _id: "f3", name: "cherry" },
   ]);
-  const [fruitObjectNew, setFruitObjectNew] = useState({
+  const [objectNew, setObjectNew] = useState({
     _id: Date.now().toString(),
     // _id: new Date().toString(),
     name: "",
-});
-console.log(fruitObjectNew)
-
+  });
+  console.log(objectNew);
+  //STRING
   const handleStringChange = (e) => {
     setStringNew(e.target.value);
   };
@@ -30,10 +26,27 @@ console.log(fruitObjectNew)
     setStringNew("");
   };
 
-  const deleteFruitString = (sringIndex) => {
+  const deleteString = (sringIndex) => {
     setStrings((strings) => {
-      return strings.filter(( str) => str !== sringIndex);
+      return strings.filter((str) => str !== sringIndex);
     });
+  };
+  // OBJECT
+  const handleObjectChange = (e) => {
+    setObjectNew({ ...objectNew, name: e.target.value });
+  };
+
+  const addObject = () => {
+    setObjects([...objects, objectNew]);
+    setObjectNew({
+      ...objectNew,
+      _id: Date.now().toString(),
+      name: "",
+    });
+  };
+  const deleteObject = (id) => {
+    const deleteObj = objects.filter((obj) => obj._id !== id);
+    setObjects(deleteObj);
   };
 
   return (
@@ -41,11 +54,7 @@ console.log(fruitObjectNew)
       <div className="strings">
         <h2>STRINGS:</h2>
         <div style={{ display: "flex", justifyContent: "center" }}>
-          <input
-            type="text"
-            value={stringNew}
-            onChange={handleStringChange}
-          />
+          <input type="text" value={stringNew} onChange={handleStringChange} />
           <button onClick={addString}>+</button>
         </div>
         <div className="items">
@@ -53,7 +62,7 @@ console.log(fruitObjectNew)
             <div key={index} className="item">
               <span className="index">{index + 1}: &nbsp;</span>
               <span className="name">{str} &nbsp;</span>
-              <button onClick={() => deleteFruitString(str)}>x</button>
+              <button onClick={() => deleteString(str)}>x</button>
             </div>
           ))}
         </div>
@@ -61,13 +70,27 @@ console.log(fruitObjectNew)
 
       <div className="objects">
         <h2>OBJECTS:</h2>
-        <div style={{ display: "flex", justifyContent: "center" }}></div>
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <input
+            type="text"
+            name="text"
+            value={objectNew.name}
+            onChange={handleObjectChange}
+          />
+          <button onClick={addObject}>+</button>
+        </div>
         <div className="items">
-          {fruitObjects.map((str, index) => (
-            <div key={str._id} className="item">
-              <div className="item">
-                {index + 1}: &nbsp;{str.name}
-              </div>
+          {objects.map((obj, index) => (
+            <div key={obj._id} className="item">
+              <span className="item">
+                {index + 1}: &nbsp;{obj.name} &nbsp;&nbsp;
+              </span>
+              <span
+                style={{ color: "red", cursor: "pointer" }}
+                onClick={() => deleteObject(obj._id)}
+              >
+                x
+              </span>
             </div>
           ))}
         </div>
